0fa114fef53fcb1bf0b7ade20a59fb879b48ddd6
jack89roberts <jroberts@turing.ac.uk>
2022-01-24 21:16:25 +0000

    black

--- boids.py
+++ boids.py
@@ -1,64 +1,73 @@
 """
-A deliberately bad implementation of [Boids](http://dl.acm.org/citation.cfm?doid=37401.37406)
+An implementation of [Boids](http://dl.acm.org/citation.cfm?doid=37401.37406)
 for use as an exercise on refactoring.
 """
 
 import random
 from numpy import array
 
-class Boid(object):
-    def __init__(self,x,y,xv,yv,owner):
-        self.position=array([x,y])
-        self.velocity=array([xv,yv])
-        self.owner=owner
 
-    def interaction(self,other):
-        delta_v=array([0.0,0.0])
-        separation=other.position-self.position
-        separation_sq=separation.dot(separation)
- 
+class Boid:
+    def __init__(self, x, y, xv, yv, owner):
+        self.position = array([x, y])
+        self.velocity = array([xv, yv])
+        self.owner = owner
+
+    def interaction(self, other):
+        delta_v = array([0.0, 0.0])
+        separation = other.position - self.position
+        separation_sq = separation.dot(separation)
+
         # Fly towards the middle
-        delta_v+=separation*self.owner.flock_attraction
-      
+        delta_v += separation * self.owner.flock_attraction
+
         # Fly away from nearby boids
-        if separation_sq < self.owner.avoidance_radius**2:
-            delta_v-=separation
+        if separation_sq < self.owner.avoidance_radius ** 2:
+            delta_v -= separation
 
         # Try to match speed with nearby boids
-        if separation_sq < self.owner.formation_flying_radius**2:
-            delta_v+=(other.velocity-self.velocity)*self.owner.speed_matching_strength
+        if separation_sq < self.owner.formation_flying_radius ** 2:
+            delta_v += (
+                other.velocity - self.velocity
+            ) * self.owner.speed_matching_strength
 
         return delta_v
 
 
-# Deliberately terrible code for teaching purposes
-class Boids(object):
-    def __init__(self,
-           flock_attraction,avoidance_radius,
-            formation_flying_radius,speed_matching_strength):
-        self.flock_attraction=flock_attraction
-        self.avoidance_radius=avoidance_radius
-        self.formation_flying_radius=formation_flying_radius
-        self.speed_matching_strength=speed_matching_strength
+class Boids:
+    def __init__(
+        self,
+        flock_attraction,
+        avoidance_radius,
+        formation_flying_radius,
+        speed_matching_strength,
+    ):
+        self.flock_attraction = flock_attraction
+        self.avoidance_radius = avoidance_radius
+        self.formation_flying_radius = formation_flying_radius
+        self.speed_matching_strength = speed_matching_strength
 
+    def initialise_random(self, count):
+        self.boids = [
+            Boid(
+                random.uniform(-450, 50.0),
+                random.uniform(300.0, 600.0),
+                random.uniform(0, 10.0),
+                random.uniform(-20.0, 20.0),
+                self,
+            )
+            for i in range(count)
+        ]
 
-    def initialise_random(self,count):
-        self.boids=[Boid(random.uniform(-450,50.0),
-                random.uniform(300.0,600.0),
-                random.uniform(0,10.0),
-                random.uniform(-20.0,20.0),self) for i in range(count)]
-
-    def initialise_from_data(self,data):
-        self.boids=[Boid(x,y,xv,yv,self) for x,y,xv,yv in zip(*data)]
+    def initialise_from_data(self, data):
+        self.boids = [Boid(x, y, xv, yv, self) for x, y, xv, yv in zip(*data)]
 
     def update(self):
         for me in self.boids:
-            delta_v=array([0.0,0.0])
+            delta_v = array([0.0, 0.0])
             for him in self.boids:
-                delta_v+=me.interaction(him)
+                delta_v += me.interaction(him)
             # Accelerate as stated
-            me.velocity+=delta_v
+            me.velocity += delta_v
             # Move according to velocities
-            me.position+=me.velocity
-
-
+            me.position += me.velocity
--- view_boids.py
+++ view_boids.py
@@ -2,26 +2,28 @@ from boids import Boids
 from matplotlib import pyplot as plt
 from matplotlib import animation
 
-boids=Boids(
-        flock_attraction=0.01/50,
-        avoidance_radius=10,
-        formation_flying_radius=100,
-        speed_matching_strength=0.125/50
-    )
+boids = Boids(
+    flock_attraction=0.01 / 50,
+    avoidance_radius=10,
+    formation_flying_radius=100,
+    speed_matching_strength=0.125 / 50,
+)
 
 boids.initialise_random(50)
 
-figure=plt.figure()
-axes=plt.axes(xlim=(-500,1500), ylim=(-500,1500))
-scatter=axes.scatter([b.position[0] for b in boids.boids],[b.position[1] for b in boids.boids])
+figure = plt.figure()
+axes = plt.axes(xlim=(-500, 1500), ylim=(-500, 1500))
+scatter = axes.scatter(
+    [b.position[0] for b in boids.boids], [b.position[1] for b in boids.boids]
+)
+
 
 def animate(frame):
     boids.update()
     scatter.set_offsets([b.position for b in boids.boids])
 
 
-anim = animation.FuncAnimation(figure, animate,
-        frames=50, interval=50)
+anim = animation.FuncAnimation(figure, animate, frames=50, interval=50)
 
 if __name__ == "__main__":
     plt.show()
