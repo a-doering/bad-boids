4deeee2cbff5a9d71904779c3ae4f5c6f71528b4
Andreas <35858164+a-doering@users.noreply.github.com>
2022-01-27 20:22:27 +0100

    Remove repetetive expression by variable

--- boids.py
+++ boids.py
@@ -3,7 +3,6 @@ import yaml
 random.seed(42)
 config = yaml.safe_load(open("config.yaml"))
 
-
 def init_boids():
 	boids_x=[random.uniform(*config["boids"]["x"]) for x in range(config["boids"]["num"])]
 	boids_y=[random.uniform(*config["boids"]["y"]) for x in range(config["boids"]["num"])]
@@ -18,28 +17,30 @@ def update_boids(boids):
 	move(xs,ys,xvs,yvs)
 
 def adjust_velocity(xs,ys,xvs,yvs):
+	num_boids = len(xs)
 	# Fly towards the middle
-	for i in range(len(xs)):
-		for j in range(len(xs)):
+	for i in range(num_boids):
+		for j in range(num_boids):
 			x_separation = (xs[j]-xs[i])
 			y_separation = (ys[j]-ys[i])
-			xvs[i]+=x_separation*config["boids"]["flock_attraction"]/len(xs)
-			yvs[i]+=y_separation*config["boids"]["flock_attraction"]/len(xs)
+			xvs[i]+=x_separation*config["boids"]["flock_attraction"]/num_boids
+			yvs[i]+=y_separation*config["boids"]["flock_attraction"]/num_boids
 			# Fly away from nearby boids
 			if x_separation**2 + y_separation**2 < config["boids"]["avoidance_radius"]**2:
 				xvs[i]-=x_separation
 				yvs[i]-=y_separation
 	# Try to match speed with nearby boids
-	for i in range(len(xs)):
-		for j in range(len(xs)):
+	for i in range(num_boids):
+		for j in range(num_boids):
 			x_separation = (xs[j]-xs[i])
 			y_separation = (ys[j]-ys[i])
 			if x_separation**2 + y_separation**2 < config["boids"]["formation_flying_radius"]**2:
-				xvs[i]+=(xvs[j]-xvs[i])*config["boids"]["speed_matching"]/len(xs)
-				yvs[i]+=(yvs[j]-yvs[i])*config["boids"]["speed_matching"]/len(xs)
+				xvs[i]+=(xvs[j]-xvs[i])*config["boids"]["speed_matching"]/num_boids
+				yvs[i]+=(yvs[j]-yvs[i])*config["boids"]["speed_matching"]/num_boids
 
 def move(xs,ys,xvs,yvs):
 	# Move according to velocities
-	for i in range(len(xs)):
+	num_boids = len(xs)
+	for i in range(num_boids):
 		xs[i]+=xvs[i]
 		ys[i]+=yvs[i]
