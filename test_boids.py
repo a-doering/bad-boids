2c62ddebdbd838e6c54864372b7e34ba00f85390
Andreas <35858164+a-doering@users.noreply.github.com>
2022-01-27 18:53:17 +0100

    Replace magic numbers with config

--- test_boids.py
+++ test_boids.py
@@ -12,6 +12,6 @@ def test_bad_boids_regression():
             assert_almost_equal(after_value,before_value,delta=0.01)
 	
 def test_bad_boids_init():
-    x,y,xv,ys = boids.init_boids()
+    x,y,xv,yv = boids.init_boids()
     assert_equal(len(x), boids.config["boids"]["num"])
     #TODO
