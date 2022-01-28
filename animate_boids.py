05e0473b7f00899d0c17f0b4d03f212a74c7d0e3
Andreas <35858164+a-doering@users.noreply.github.com>
2022-01-28 09:26:01 +0100

    Update animation to objects

--- animate_boids.py
+++ animate_boids.py
@@ -1,18 +1,20 @@
-import boids as boids_lib
+import boids as bd
 from matplotlib import pyplot as plt
 from matplotlib import animation
 
-config=boids_lib.config
-boids = boids_lib.init_boids()
+config=bd.config
+boids = bd.Boids(**config["boids"])
+boids.initialize_boids()
 
 figure=plt.figure()
 axes=plt.axes(xlim=(config["plot"]["xlim"][0], config["plot"]["xlim"][1]), ylim=(config["plot"]["ylim"][0], config["plot"]["ylim"][1]))
-scatter=axes.scatter(boids[0],boids[1])
+scatter=axes.scatter(
+   [b.position[0] for b in boids.boids], [b.position[1] for b in boids.boids]
+   )
 
 def animate(frame):
-   boids_lib.update_boids(boids)
-   scatter.set_offsets(list(zip(boids[0],boids[1])))
-
+   boids.update_boids()
+   scatter.set_offsets([b.position for b in boids.boids])
 
 anim = animation.FuncAnimation(figure, animate,
                                frames=config["plot"]["frames"], interval=config["plot"]["interval"])
